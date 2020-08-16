b4e04f7187e32596b26f2d1cff5d7e63ed2e655c
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-08-16 14:42:25 +0200

    Update index.php

--- phpcodeexamples/consolelogwithphp/index.php
+++ phpcodeexamples/consolelogwithphp/index.php
@@ -8,4 +8,5 @@ function console_log( $string ){
 
 $myvar = "EZ EGY PHP VÁLTOZÓBAN LÉVŐ STRING, AMIT A BÖNGÉSZŐ KONZOLLOGJÁBA SZERETNÉK KIÍRATNI";
 console_log( $myvar );
+echo 'Nyisd meg a devtoolst, és nézd meg az üzenetet a konzol naplóban';
 ?>
