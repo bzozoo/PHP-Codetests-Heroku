854cdeb2fe40e0e059af009c9974aaed4bdaafda
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-08-16 13:29:18 +0200

    Update index.php

--- phpcodeexamples/consolelogwithphp/index.php
+++ phpcodeexamples/consolelogwithphp/index.php
@@ -1,1 +1,10 @@
-Welcome
+
+function console_log( $string ){
+  echo '<script>';
+  echo 'console.log('.  $string  .')';
+  echo '</script>';
+}
+
+
+$myvar = "EZ EGY PHP VÁLTOZÓBAN LÉVŐ STRING, AMIT A BÖNGÉSZŐ KONZOLLOGJÁBA SZERETNÉK KIÍRATNI";
+console_log( $myvar );
