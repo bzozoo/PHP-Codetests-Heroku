7d0ec89d762238316fc4516d5d8a163c28c98ca7
bzozoo <47896536+bzozoo@users.noreply.github.com>
2021-02-20 14:14:21 +0100

    Add files via upload

--- phpcodeexamples/SessionTests/1/index.php
+++ phpcodeexamples/SessionTests/1/index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
-$_SESSION["teszts"] = "Ez új egy teszt érték";
+$_SESSION["teszts"] = "Ez új egy teszt érték!";
 
-echo $_SESSION["teszts"];
+$_SESSION["teszts2"] = "Ez egy másik teszt érték!";
+
+echo "S_SESSION [teszts] = " . $_SESSION['teszts'] . " - S_SESSION [teszts2] = " . $_SESSION['teszts2'];
