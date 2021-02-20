bbdded71824fc40245a9542e64210f22e365ea1c
bzozoo <47896536+bzozoo@users.noreply.github.com>
2021-02-20 20:16:07 +0100

    Add files via upload

--- phpcodeexamples/SessionTests/2/opensession.php
+++ phpcodeexamples/SessionTests/2/opensession.php
@@ -6,7 +6,13 @@ header('Content-Type: application/json');
 
 if(isset($_COOKIE["PHPSESSID"])){
 	session_start();
-	$data = Array("DataQuery" => "Success", "ActualSessionID" => session_id(), "SessionTime" => $_SESSION["SessionTime"], "SessionValue" => $_SESSION["SessionValue"]);
+	$data = Array(
+	     "DataQuery" => "Success",
+		 "ActualSessionID" => session_id(),
+		 "SessionTime" => $_SESSION["SessionTime"],
+		 "SessionUserName" => $_SESSION["SessionUname"],
+		 "SessionValue" => $_SESSION["SessionValue"],
+		 "DirectAccess" => True);
 	echo json_encode($data);
 	exit;
 }
@@ -19,15 +25,33 @@ session_id($thesessionid);
 session_start();
 
 if(isset($_SESSION["SessionTime"]) || isset($_SESSION["SessionValue"])){
-$data = Array("DataQuery" => "Success", "ActualSessionID" => session_id(), "SessionTime" => $_SESSION["SessionTime"],  "SessionUserName" => $_SESSION["SessionUname"], "SessionValue" => $_SESSION["SessionValue"]);
+	
+$data = Array(
+      "DataQuery" => "Success",
+	  "ActualSessionID" => session_id(),
+	  "SessionTime" => $_SESSION["SessionTime"],
+	  "SessionUserName" => $_SESSION["SessionUname"],
+	  "SessionValue" => $_SESSION["SessionValue"],
+	  "DirectAccess" => False);
+
 } else {
-	$data = Array("DataQuery" => "Failed","ActualSessionID" => "Failed", "SessionTime" => "Failed", "SessionUserName" => "Failed", "SessionValue" => "Failed");
+	$data = Array(
+	      "DataQuery" => "Failed",
+		  "ActualSessionID" => "Failed",
+		  "SessionTime" => "Failed",
+		  "SessionUserName" => "Failed",
+		  "SessionValue" => "Failed");
 }
 
 
 } else {
 
-$data = Array("DataQuery" => "Failed","ActualSessionID" => "Failed", "SessionTime" => "Failed",  "SessionUserName" => "Failed", "SessionValue" => "Failed");
+$data = Array("
+       DataQuery" => "Failed",
+	   "ActualSessionID" => "Failed",
+	   "SessionTime" => "Failed",
+	   "SessionUserName" => "Failed",
+	   "SessionValue" => "Failed");
 }
 
 echo json_encode($data);
