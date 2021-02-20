54c4b05d66f81282b67b81766afcab0fc8f2f107
bzozoo <47896536+bzozoo@users.noreply.github.com>
2021-02-20 19:37:36 +0100

    Add files via upload

---
+++ phpcodeexamples/SessionTests/2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+session_start();
+
+$_SESSION["SessionTime"] = time();
+
+$_SESSION["SessionValue"] = "Ez egy teszt érték!";
+
+$data = Array("SessionID" => session_id(), "SessionTime" => $_SESSION["SessionTime"], "SessionValue" => $_SESSION["SessionValue"]);
+
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+echo json_encode($data);
