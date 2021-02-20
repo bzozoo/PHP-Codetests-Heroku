e741a5f5879f3e84ae1f02bf38071ab6d027a5da
bzozoo <47896536+bzozoo@users.noreply.github.com>
2021-02-20 23:19:18 +0100

    Update index.php

--- phpcodeexamples/easybackend/index.php
+++ phpcodeexamples/easybackend/index.php
@@ -9,19 +9,19 @@ if(isset($_POST['kapottadat']) && is_numeric($_POST['kapottadat'])){
 	
 	$kapottSzamAdat = $_POST['kapottadat'];
 	$data = Array(
-	    'Dupla' => $kapottSzamAdat * 2,
+	        'Dupla' => $kapottSzamAdat * 2,
 		'Felez' => $kapottSzamAdat / 2,
 		'Hozzaad' => $kapottSzamAdat + 1,
 		'Levon' => $kapottSzamAdat -1,
 		'Hello' => "Hello " . $kapottSzamAdat . "!",
 		'Visszakap' => $kapottSzamAdat,
 		'Error' => false
-	);
+	       );
 	
 	echo json_encode($data, JSON_PRETTY_PRINT);
 } else {
 	$data = Array(
-	'Error' => true
-	);
+	    'Error' => true
+	     );
 	echo json_encode($data, JSON_PRETTY_PRINT);
 }
